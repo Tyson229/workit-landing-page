0dde9d8e48bdf73652fec825fe02c14cfaa99425
Khoa <ngyntienkhoa22999@gmail.com>
2023-10-19 17:42:38 +0700

    finish

--- src/components/person/InfoBox.tsx
+++ src/components/person/InfoBox.tsx
@@ -13,7 +13,7 @@ const InfoBox = () => {
       >
         Hi, I'm Louis Graham, the founder of the company. Book a demo call with
         me to become a beta tester for our app and kickstart your company. Apply
-        for access below and I’ll be in touch to schedule a call.
+        for access below and I'll be in touch to schedule a call.
       </p>
       <PrimaryButton title="Apply for access" />
     </div>
